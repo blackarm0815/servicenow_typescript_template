cafb8a60df2fe81d7b2774a79b8090eec3bf7ba4
magnus <magnusmichaelson@protonmail.com>
2024-08-13 16:40:31 +0100

    adapterTester - prettied up test.js

--- test.js
+++ test.js
@@ -1,9 +1,65 @@
-var foo = {};
+// current state of tests
+//
+// reportData[testNetworkAdapterSysId] = {
+//
+//
+//
+//
+//   testMainCiNotEmpty: testMainCiNotEmpty(testNetworkAdapterSysId),
+//   testMainCiValid: testMainCiValid(testNetworkAdapterSysId),
+//   testMainConnectedCiMatchesConnectedPortCi: testMainConnectedCiMatchesConnectedPortCi(testNetworkAdapterSysId),
+//   testMainConnectedCiNotEmpty: testMainConnectedCiNotEmpty(testNetworkAdapterSysId),
+//   testMainConnectedCiValid: testMainConnectedCiValid(testNetworkAdapterSysId),
+//   testMainConnectedPortNotEmpty: testMainConnectedPortNotEmpty(testNetworkAdapterSysId),
+//   testMainConnectedPortValid: testMainConnectedPortValid(testNetworkAdapterSysId),
+//
+//
+//
+//
+//   testConnectedPortFieldsAlreadyMatchMainFields: false,
+//   testConnectedPortCiNotEmpty: false,
+//   testConnectedPortCiValid: false,
+//   testConnectedPortConnectedCiNotEmpty: false,
+//   testConnectedPortConnectedCiValid: false,
+//   testConnectedPortConnectedPortNotEmpty: false,
+//   testConnectedPortConnectedPortValid: false,
+//
+//
+//
+//
+//   urlNetworkAdapter: `${url}cmdb_ci_network_adapter.do?sys_id=${testNetworkAdapterSysId}`,
+// };
+//
+//
+//
+//
+var encodedQuery = 'cmdb_ciISNOTEMPTY^u_switchISNOTEMPTY^u_switchportISNOTEMPTY';
+var queryLimit  = '10';
+//
+//
+//
+//
+var cmdbCiNetworkAdapterReport = {};
+//
+//
+//
 //
 var checkSysId = function (testSysId) {
+  //
+  var whiteSpaceRemoved = '';
+  //
+  // the type is string
   if (typeof testSysId === 'string') {
-    if (testSysId !== '') {
-      return testSysId;
+    // copy the test sys_id to a string that will have all whitespace removed
+    whiteSpaceRemoved = testSysId;
+    whiteSpaceRemoved.replace(' ', '');
+    whiteSpaceRemoved.replace(/\t/g, '');
+    // the string contains non whitespace characters
+    if (whiteSpaceRemoved !== '') {
+      // the string is 32 characters long. standard for a servicenow sys_id
+      if (testSysId.length === 32) {
+        return testSysId;
+      }
     }
   }
   return null;
@@ -15,8 +71,8 @@ var getTestData = function () {
   var checkAdapterSysId;
   //
   grTestAdapters = new GlideRecord('cmdb_ci_network_adapter'); 
-  grTestAdapters.addEncodedQuery('cmdb_ciISNOTEMPTY^u_switchISNOTEMPTY^u_switchportISNOTEMPTY');
-  grTestAdapters.setLimit(10);
+  grTestAdapters.addEncodedQuery(encodedQuery);
+  grTestAdapters.setLimit(queryLimit);
   grTestAdapters.query();
   while (grTestAdapters.next()) {
     checkAdapterSysId = checkSysId(grTestAdapters.getUniqueValue());
@@ -32,13 +88,13 @@ var getTestData = function () {
 var showReport = function () {
   var reportText = '';
   reportText += '<pre>';
-  reportText += JSON.stringify(foo, null, 2);
+  reportText += JSON.stringify(cmdbCiNetworkAdapterReport, null, 2);
   reportText += '</pre>';
     gs.print(reportText); 
 };
 var networkAdapterSysIdArray = getTestData();
 //
 // get report from the script include
-foo = magnusCmdbCiNetworkAdapterReport(networkAdapterSysIdArray);
+cmdbCiNetworkAdapterReport = magnusCmdbCiNetworkAdapterReport(networkAdapterSysIdArray);
 //
 showReport();
